430882a5662aab1877cf7ae2892dfc4745d77757
greengreen2212 <101193930+greengreen2212@users.noreply.github.com>
2022-04-11 15:39:17 +0530

    Update retry_ccache.sh

--- retry_ccache.sh
+++ retry_ccache.sh
@@ -28,7 +28,7 @@ retry_ccache () {
 	export CCACHE_EXEC=$(which ccache)
 	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
 	if [ $hit_rate -lt 99 ]; then
-		git clone https://${TOKEN}@github.com/geopd/Builder-CI-A12 cirrus && cd $_
+		git clone https://${TOKEN}@github.com/greengreen2212/Builder-CI-A12 cirrus && cd $_
 		git commit --allow-empty -m "Retry: Ccache loop $(date -u +"%D %T%p %Z")"
 		git push -q
 	else
