9e25c4d2704b595df1cd3c6472bccafc95924477
greengreen2212 <101193930+greengreen2212@users.noreply.github.com>
2022-04-13 07:00:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ build_command() {
 		;;
 		"Crdroid-12") lunch lineage_sakura-user && m bacon -j20
 		;;
-		"lineage-19.1") mka sepolicy && mka init && mka bootimage && mka bacon -j20
+		"lineage-19.1") mka showcommands && mka sepolicy && mka init && mka bootimage && mka bacon -j20
 		;;
 		"Evox-12") lunch evolution_sakura-user && m evolution -j20
 		;;
